8cb39c15d7004823fe2d6ffb347dcd17f67708f6
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2023-11-10 20:50:01 +1030

    hotjar tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,12 +27,24 @@ export default function RootLayout({
             <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VTDSFE7JPH" />
             <Script id='google-analytics' strategy='afterInteractive'>
               {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VTDSFE7JPH');
-  `}
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                tag('config', 'G-VTDSFE7JPH');
+              `}
+            </Script>
+            {/* <!-- Hotjar Tracking Code for https://www.isaxcode.com/ --> */}
+            <Script id='hotjar-tracking' strategy='afterInteractive'>
+              {`
+                (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3730718,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+               })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `}
             </Script>
             {children}
             <Analytics />
