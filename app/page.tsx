a6c34c4e4d3a7f728875377140bb230c39dcc908
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2023-11-16 10:31:20 +1030

    Lazy Loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import type {Metadata, NextPage } from "next";
-import ContactUs from "../components/contactus";
-import Footer from "../components/Footer";
+// import ContactUs from "../components/contactus";
+// import Footer from "../components/Footer";
 import Masthead from "../components/Masthead";
 import Skills from "../components/Skills";
-import TrustedBy from "../components/Trustedby";
+// import TrustedBy from "../components/Trustedby";
 import Works from "../components/Works";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const TrustedBy = dynamic(() => import('../components/Trustedby'), {
+  loading: () => <p>Loading...</p>,
+})
+
+
+const ContactUs = dynamic(() => import('../components/contactus'), {
+  loading: () => <p>Loading...</p>,
+})
+
+const Footer = dynamic(() => import('../components/Footer'), {
+  loading: () => <p>Loading...</p>,
+})
 
 const Home: NextPage = () => {
   return (
