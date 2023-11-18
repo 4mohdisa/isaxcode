3c9dc900806026b7e6356d7abafe05ddd0a5f517
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2023-11-18 16:53:10 +1030

    images compressed

--- components/Works.tsx
+++ components/Works.tsx
@@ -37,8 +37,7 @@ const Works = () => (
             <WorkRight progress={progress}>
               <Image
                 src="/assets/works/graphic.png"
-                width={840}
-                height={1620}
+                fill
                 alt="Visual communication experts, freelance graphic design services, logo design specialists, branding consultants, packaging design artists, illustration experts, web design professionals, UI/UX design aficionados, motion graphics specialists, photo editing masters, digital marketing strategists, social media marketing gurus, custom logo design services, innovative logo redesign experts, logo branding specialists, creative logo creation experts, logo development professionals, logo concept pioneers, logo sketching artists, logo iteration masters, logo finalization experts, logo file providers, comprehensive logo guidelines creators, logo usage consultants, branding material design consultants, transformative branding services, brand identity design specialists, comprehensive brand guidelines creators, detailed brand style guide experts, brand voice specialists, impactful brand messaging experts, brand personality consultants, effective brand positioning experts, strategic brand development experts, brand consistency consultants, brand management gurus, brand awareness experts, brand equity enhancers, brand loyalty specialists, custom poster design experts, creative poster creation artists, innovative poster development professionals, conceptual poster design pioneers, skilled poster sketching artists, masterful poster iteration experts, finalization-ready poster specialists, professional poster printing consultants, effective poster distribution experts, strategic poster marketing consultants, impactful poster advertising specialists, talented freelancers, freelance graphic design specialists, skilled freelance copywriters, expert freelance logo designers, knowledgeable freelance branding consultants, creative freelance poster designers, proficient freelance website design and development experts, experienced freelance digital marketing strategists, savvy freelance social media marketing experts, engaging freelance content marketing specialists, effective freelance SEO copywriters, persuasive freelance web copywriters, creative freelance social media copywriters, compelling freelance email copywriters, impactful freelance advertising copywriters, innovative freelance creative copywriters, technical freelance copywriters, business-oriented freelance copywriters, legal freelance copywriters, medical freelance copywriters
 
 "
@@ -90,13 +89,11 @@ const Works = () => (
             <WorkRight progress={progress}>
               <div className="w-full max-w-md pt-10 lg:pt-0 px-10 md:px-0">
                 <Image
-                  src="/assets/works/CopyWriting.svg"
+                  src="/assets/works/CopyWriting.png"
                   loading="lazy"
-                  width={840}
-                  height={1620}
-                  alt="Persuasive copywriters, freelance copywriting services, compelling website copy specialists, engaging blog post writers, skilled social media content creators, effective email marketing copywriters, impactful advertising copywriters, creative SEO copywriting experts, experienced copyediting and proofreading professionals, talented freelance blog writers, captivating blog post creators, engaging content strategists, informative article writers, knowledgeable blog topic researchers, skilled blog post optimizers, effective blog promotion experts, social media-savvy blog writers, experienced freelancers, freelance copywriting specialists, expert blog writers, creative content marketers, SEO-savvy content creators, social media management experts, email marketing gurus, digital marketing strategists, skilled web designers and developers.
-
-                "
+                  fill
+                  alt="Persuasive copywriters, freelance copywriting services, compelling website copy specialists, engaging blog post writers, skilled social media content creators, effective email marketing copywriters, impactful advertising copywriters, creative SEO copywriting experts, experienced copyediting and proofreading professionals, talented freelance blog writers, captivating blog post creators, engaging content strategists, informative article writers, knowledgeable blog topic researchers, skilled blog post optimizers, effective blog promotion experts, social media-savvy blog writers, experienced freelancers, freelance copywriting specialists, expert blog writers, creative content marketers, SEO-savvy content creators, social media management experts, email marketing gurus, digital marketing strategists, skilled web designers and developers"
+                  style={{ objectFit: "cover" }}
                 />
               </div>
             </WorkRight>
