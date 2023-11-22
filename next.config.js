5642f145bcf81817c912295d545909e3e2974ee0
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2023-11-22 17:09:21 +1030

    fixes

--- next.config.js
+++ next.config.js
@@ -22,6 +22,10 @@ module.exports = {
           protocol: 'https',
           hostname: 'images.unsplash.com'
         },
+        {
+          protocol: 'https',
+          hostname: 'wp.isaxcode.com'
+        }
       ],
     },
   }
