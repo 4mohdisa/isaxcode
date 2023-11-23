aaaae637c6d959435ab43ba2d5b83f115bb6a8e0
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2023-11-23 14:24:16 +1030

    reusable

---
+++ components/backButton.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/shadcn-ui/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const BackButton = () => {
+  return (
+    <Link href="javascript:history.back()" className='hidden lg:inline'>
+    <Button className="fixed top-0 left-10 mt-10 w-auto h-auto !rounded-full !z-50 "><Image width={50} height={50} alt="back-icon" src="/assets/back.svg" /></Button>
+    </Link>
+  )
+}
+
+export default BackButton
