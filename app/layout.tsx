25a17bc59f511019e292888434f9878f51bc0b1b
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2023-12-13 15:41:13 +1030

    <SpeedInsights />

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from 'next'
 import "../styles/globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import ScrollObserver from "../utils/scroll-oberver";
 import SizeObserver from "../utils/size-observer";
 import Script from "next/script";
@@ -53,6 +53,7 @@ export default function RootLayout({
             </Script>
             {children}
             <Analytics />
+            <SpeedInsights />
           </body>
         </html>
       </ScrollObserver>
